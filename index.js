1c5de8280ff3a1a42718efc51cd8c603f383ba01
HannaKazello <hanna.kazello@gmail.com>
2017-06-14 17:13:30 +0300

    remove unnecessary comment, add new functions

--- index.js
+++ index.js
@@ -1,137 +1,56 @@
-//need to change processItems and run functions in alis-web-request module(from alis-web-request -dev )
-//function processItems(options, callback) {
-//   if (!options) {
-//     return process.nextTick(callback, new Error('options is not provided'));
-//   }
-//
-//   getPage({ url: options.url, jar: options.jar }, (err, body) => {
-//     const $ = parsePage(body);
-//     const items = getItems($);
-//     const nextPageUrl = getNextPageUrl($);
-//
-//     callback(null, nextPageUrl, items);
-//   });
-// }
-//
-// function run(fn, q, options, callback) {
-//   if (!q) {
-//     return new Error('q is not provided');
-//   }
-//
-//   if (q[0] === 'undefined') {
-//     return;
-//   }
-//
-//   fn({ url: `${options.alisEndpoint}/alis/EK/${q[0]}`, jar: options.jar }, (err, nextPageUrl, items) => {
-//     if (err) {
-//       return err;
-//     }
-//
-//     const remainingQueue = q.slice(1);
-//     if (q.length === 1) {
-//       remainingQueue.push(`${nextPageUrl}`);
-//     }
-//
-//     callback(null, items);
-//     run(fn, remainingQueue, options, callback);
-//   });
-// }
-
-
-//add this to alis-web-request module
-// function returnPagesItems(options, pageNumber, total, callback){
-//   if (!options) {
-//     return process.nextTick(callback, new Error('provided is not provided'));
-//   }
-//   if (!pageNumber) {
-//     return process.nextTick(callback, new Error('pageNumber is not provided'));
-//   }
-//   if (pageNumber>total/20) {
-//     return process.nextTick(callback, new Error('page doesnt exist'));
-//   }
-//   processItems({ url: `${options.alisEndpoint}/alis/EK/do_other.php?frow=1&fcheck=1&ccheck=1&action=${pageNumber}&crow=1`, jar: options.jar}, (err, nextPageUrl, books) => {
-//     if (err) {
-//       return callback(err);
-//     }
-//     return callback(null,books, nextPageUrl);
-//   });
-// }
-
 
 import elasticsearch from 'elasticsearch';
 import Stream from 'stream';
-import { sendInitialQuery, getNumberedPageUrls, run, processItems, parsePage, getTotal, returnPagesItems} from '@grodno-city/alis-web-request';
+import { getRecordsByQuery} from '@grodno-city/alis-web-request';
 
-// var client = new elasticsearch.Client({
-//   host: 'localhost:9200',
-//   log: 'trace'
-// });
-// client.ping({
-//   // ping usually has a 3000ms timeout
-//   requestTimeout: 1000
-// }, function (error) {
-//   if (error) {
-//     console.trace('elasticsearch cluster is down!');
-//   } else {
-//     console.log('All is well');
-//   }
-// });
+let client = new elasticsearch.Client({
+  host: 'localhost:9200',
+});
 
-
-const initParams = {
-  year: 1960,
-  alisEndpoint: 'http://86.57.174.45',
-};
-
-sendInitialQuery(initParams, (err, res) => {
-  if (err) {
-    return new Error(err);
+function indexAllRecordsByYear(year) {
+  const options={
+    query: year,
+    queryType: 'Год издания',
+    recordsType: 'Все',
+    alisEndpoint: 'http://11.11.11.11'
   }
-  console.log('in in send');
-  const options = {
-    alisEndpoint: initParams.alisEndpoint,
-    jar: res.jar
-  };
-  const $ = parsePage(res.page);
-  const firstNumberedPageUrls = getNumberedPageUrls($);
-  let remainingQueue = firstNumberedPageUrls;
-  let total = getTotal($);
-  console.log('total', total);
-  // run(processItems, remainingQueue, options, (err, items)=>{
-  //   if (err) {
-  //     return err;
-  //   }
-  //
-  // });
-  let pageNumber = 2;
-  returnPagesItems(options, pageNumber, total, (err, items, nextPageUrl)=>{
-    if(!err){
-      remainingQueue.splice(pageNumber-1, 1);
-      if (remainingQueue.length === 1) {
-        remainingQueue.push(`${nextPageUrl}`);
-      }
-      indexItems(items);
-      console.log('q : ', remainingQueue);
-
+  getRecordsByQuery(options, (err, memo)=>{
+    if(!err) {
+      memo.map((item)=>{
+        indexItem(item, 'Все', year);
+      })
     }
-    return err;
-//do_other.php?frow=1&fcheck=1&ccheck=1&action=3&crow=1
+  });
+}
 
-});
+// client.index({
+//   index: 'records',
+//   id:'H87733338',
+//   type: 'Книги',
+//   body:{
+//         "title": "Чехов, М. П. Вокруг Чехова : встречи и впечатления / М. П. Чехов ; вступительная статья Е. З. Балабановича. - Москва : Московский рабочий, 1960. - 351 с. ББК 83.3(2=Рус)1",
+//         "year": 1960
+//       }
+// })
 
-});
+function getDocument(id, type, callback){
+  client.get({
+    index: 'records',
+    type: type,
+    id: id
+  }, callback);
+}
 
-function indexItems(books){
-  console.log('books: ', books);
-  //create index here
-  // books.map((item)=>{
-  //   client.index({
-  //     index: 'book',
-  //     type: 'temp',
-  //     body:{
-  //       'id':item.id,
-  //       'title': item.title
-  //     },
-  //   }).then(function(body){console.log('body',body)})
-  // })
+function indexItem(item, type, year){
+  client.index({
+    index: 'records',
+    id:item.id,
+    type: type,
+    body:{
+      'title': item.title,
+      'year': year,
+    }
+  }, (err, response) => {
+    if (err) throw err;
+  })
 }
