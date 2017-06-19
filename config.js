d9b34a9188c77c9a8c64a6b8e2c97d6d15b60b7b
HannaKazello <hanna.kazello@gmail.com>
2017-06-19 17:28:09 +0300

    Extract `indexByYear` function from consumer script #6 (#8)
    * add async to depend.
    
    * Extract `indexByYear` function from consumer script #6
    
    * Remove debug statement from indexByType fn

---
+++ config.js
@@ -0,0 +1,11 @@
+import elasticsearch from 'elasticsearch';
+import bunyan from 'bunyan';
+
+export const client = new elasticsearch.Client({
+  host: 'localhost:9200',
+});
+
+export const log = bunyan.createLogger({ name: 'index' });
+
+export const alisEndpoint = 'http://86.57.174.45';
+export const index = 'records';
--- consumer.js
+++ consumer.js
@@ -1,35 +1,3 @@
-import { indexRecordsByQuery } from './index';
-import elasticsearch from 'elasticsearch';
-import bunyan from 'bunyan';
-import { recordTypes } from '@grodno-city/alis-web-request';
-import eachOfSeries from 'async/eachOfSeries';
+import { indexByYear } from './index';
 
-const client = new elasticsearch.Client({
-  host: 'localhost:9200',
-});
-
-let log = bunyan.createLogger({ name: 'consumer' });
-let options={
-  query: 2009,
-  queryType: 'Год издания',
-  recordType: 'Все',
-  index: 'records',
-  alisEndpoint: 'http://86.57.174.45',
-}
-let types = Object.keys(recordTypes);
-types.shift();
-function indexByType(value,key,callback){
-  options.recordType = value;
-  indexRecordsByQuery(client, options, (err)=>{
-    if (!err) return callback();
-    if(err.message=='no match') {
-      log.warn(value, err.message)
-      callback();
-    }
-    else callback(err);
-  });
-}
-
-eachOfSeries(types, indexByType, (err)=>{
-  if(err) log.warn(err.message);
-})
+indexByYear(1999);
