371569bff1918aca6e43a9a42be959a6cb5ec76d
HannaKazello <hanna.kazello@gmail.com>
2017-07-24 17:45:06 +0300

    add indexRecord to consumer.js

--- consumer.js
+++ consumer.js
@@ -1,9 +1,30 @@
+import elasticsearch from 'elasticsearch';
+import { index, elasticHost, elasticPort } from './config.json';
+
+const client = new elasticsearch.Client({
+  host: `${elasticHost}:${elasticPort}`,
+});
+
+function indexRecord(record, callback) {
+  client.index({
+    index,
+    type: 'info',
+    id: record.id,
+    body: {
+      record,
+    },
+  }, callback);
+}
+
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
     if (chunk[0] !== 10) {
       const data = JSON.parse(chunk);
-      console.log(data.msg);
+      if (data.msg === 'OK') {
+        // console.log(data.record);
+        indexRecord(data.record, () => {});
+      }
     } else  console.log(chunk[0]);
   }
 });
